aca61dedb036eccdbe0c1dc5a1e99d92e6bbbffb
anastasiia <repalo.a@gmail.com>
2021-05-14 15:24:17 +0300

    written 2a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cin >> s;
+
+    int cnt_a = 0;
+    int cnt_a_not_b = 0;
+
+    for(int i = 0;i < s.size();++i) {
+        if(s[i] == 'a') {
+            cnt_a++;
+            if(i+1 >= s.size() || s[i+1] != 'b')
+                cnt_a_not_b++;
+        }
+    }
+    cout << cnt_a << ' ' << cnt_a_not_b << '\n';
     return 0;
-з}
+}
